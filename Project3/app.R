1b6cbf81dcc173f919f63486cc6a9a3e920d0b39
Lee Pixton <ldpixton@ncsu.edu>
2021-07-30 01:04:31 -0400

    completed the 'about' tab and almost completed 'data'

--- Project3/app.R
+++ Project3/app.R
@@ -1,5 +1,9 @@
 library(shiny)
 library(shinydashboard)
+library(tidyverse)
+library(DT)
+
+data <- read.csv("../Project3/baseball.csv")
 
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
@@ -20,12 +24,111 @@ ui <- dashboardPage(
         )
     ),
 
-    dashboardBody()
+    dashboardBody(tabItems(
+        # About tab content
+        tabItem(tabName = "about",
+                h1("About the Dashboard"),
+                fluidRow(
+                    box(
+                        HTML("<p>This applet allows for exploration into what factors lead to Home Runs (HR) in baseball. You can do this through exploring the data, altering the data set, and modeling using multiple different methods. The data set includes a number of variables about both the individual match-up of pitcher vs. batter, as well as the ballpark dimensions which often play a large part in the number of HR hit. This <a href='https://www.kaggle.com/jcraggy/baseball'>data set</a> comes from <a href='https://www.kaggle.com/'>kaggle</a>, a popular data science competition and collaboration website.</p>"),
+                        uiOutput("about"),
+                        br(),
+                        HTML("<p> Now dig into the data and see what you can find!</p>")
+                        ),
+                    box(imageOutput('mlb'))
+                )
+        ),
+        
+        # Data tab content
+        tabItem(tabName = "data",
+                h1("Data"),
+                fluidRow(
+                    box(width=3,
+                        checkboxInput("change", "Adjust the data set?"),
+                        conditionalPanel(
+                            'input.change == true',
+                            checkboxGroupInput("show_vars", "Columns in data set to show:",
+                                                           names(data), selected = names(data), inline=FALSE),
+                            actionButton("generateButton","Save Data")
+                        )
+                    ),
+                    box(width=9,dataTableOutput("dataTable"))
+                )
+                
+        ),
+        
+        # Data Exploration tab content
+        tabItem(tabName = "explore",
+                fluidRow(
+                    box(),
+                    box()
+                )
+        ),
+        
+        # Modeling tab content
+        tabItem(tabName = "model",
+                fluidRow(
+                    box(),
+                    box()
+                )
+        ),
+        
+        # Model info tab content
+        tabItem(tabName = "info",
+                fluidRow(
+                    box(),
+                    box()
+                )
+        ),
+        
+        # Model fitting tab content
+        tabItem(tabName = "fit",
+                fluidRow(
+                    box(),
+                    box()
+                )
+        ),
+        
+        # Prediction tab content
+        tabItem(tabName = "predict",
+                fluidRow(
+                    box(),
+                    box()
+                )
+        )
+    )
+)
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-
+    getData <- reactive({
+        newData <- data[, input$show_vars]
+    })
+    
+    output$about <- renderUI({
+        HTML(paste("There are multiple tabs on the left.","", "The 'Data' tab allows you to explore and filter the data as desired, and gives the option to save the new data set to a CSV file.", "",
+              "The 'Data Exploration' tab contains multiple ways to summarize and visualize the data.", "", "The 'Modeling' tab is where the statistical modeling will be done. This tab contains multiple sub-tabs:",
+              "&emsp; 1) A 'Modeling Info' tab which allows you to better understand the techniques.","&emsp; 2) A 'Model Fitting' tab where the models are actually fit and settings for each model are adjusted.",
+              "&emsp; 3) A 'Prediction' tab, where the models can then be used on a prediction set or observation, and the results displayed.",sep="<br/>"))
+    })
+    
+    output$mlb <- renderImage({
+        list(src = 'mlb.png',
+             contentType = 'image/png',
+             width = "100%",
+             height = "100%",
+             alt = "This is alternate text")
+    }, deleteFile = FALSE)
+    
+    output$dataTable <- renderDataTable(
+        getData(),
+        filter = "top",
+        options = list(scrollX = TRUE,lengthMenu = c(15, 30, 50, 100), pageLength = 10)
+    )
+    
+    observe({if(input$generateButton == 1) {write.csv(getData(),"new_baseball.csv", row.names = FALSE, na = "")}})
+    
 }
 
 # Run the application 
